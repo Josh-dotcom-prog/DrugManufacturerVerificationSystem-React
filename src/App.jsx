56a37c8ba60e26dfcfe90a716fa838244fda86c2
Josh-dotcom-prog <edyangujoshua@gmail.com>
2025-04-15 16:58:14 +0300

    updated file names

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,9 @@
 import React from "react"
 import Navbar from "./Components/Navbar/Navbar"
 import Hero from "./Components/Hero/Hero"
-import Footer from "./Components/Footer/Footer"
 import Login from './Components/Auth/Login';
 import Signup from './Components/Auth/Signup';
 import Manufacturers from './Components/Manufacturers/Manufacturers';
-import Sidebar from "./Components/Sidebar/Sidebar";
 import { BrowserRouter as Router, Routes, Route, Navigate, useNavigate, useParams, useLocation } from 'react-router-dom';
 
 const App = () => {
@@ -20,11 +18,9 @@ const App = () => {
             <Route path="/login" element={<Login />} />
             <Route path="/signup" element={<Signup />} />
             <Route path="/manufacturers" element={<Manufacturers />} />
-            <Route path="/sidebar" element={<Sidebar />} />
             <Route path="*" element={<Navigate to="/" replace />} />
           </Routes>
         </div>
-        <Footer />
       </div>
     </Router>
   )
